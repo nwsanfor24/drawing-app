f984d0488b746a3de2471c9db5f29d7beea4b85a
Nathan Sanford <nwsanfor24@gmail.com>
2021-02-16 11:36:09 -0500

    js finished

--- script.js
+++ script.js
@@ -47,3 +47,39 @@ function drawCircle(x, y) {
     ctx.fill()
 }
 
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
+}
+
+function updateSizeOnScreen() {
+    sizeEl.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if (size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if (size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
